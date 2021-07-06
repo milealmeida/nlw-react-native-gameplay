d39caa211884f36d87dfe8b5f426ca83ed3f85f1
Milena <milenaalmeidadev@hotmail.com>
2021-07-06 09:55:06 -0400

    class#02 finished

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,6 +5,8 @@ import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
 import { Profile } from '../../components/Profile';
 import { ListHeader } from '../../components/ListHeader';
+import { Appointment } from '../../components/Appointment';
+import { ListDivider } from '../../components/ListDivider';
 
 import { styles } from './styles';
 
@@ -14,6 +16,18 @@ export function Home() {
     const appointments = [
         {
             id: '1',
+            guild: {
+                id: '1',
+                name: 'Legendários',
+                icon: null,
+                owner: true,
+            },
+            category: '1',
+            date: '22/06 às 20:40h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+        },
+        {
+            id: '2',
             guild: {
                 id: '1',
                 name: 'Legendários',
@@ -37,12 +51,10 @@ export function Home() {
                 <ButtonAdd />
             </View>
 
-            <View>
-                <CategorySelect 
-                    categorySelected={category}
-                    setCategory={handleCategorySelect}
-                />
-            </View>
+            <CategorySelect 
+                categorySelected={category}
+                setCategory={handleCategorySelect}
+            />
 
             <View style={styles.content}>
                 <ListHeader
@@ -54,8 +66,11 @@ export function Home() {
                     data={appointments}
                     keyExtractor={item => item.id}
                     renderItem={( {item }) => (
-                        <Text>{item.guild.name}</Text>
+                        <Appointment data={item} />
                     )}
+                    ItemSeparatorComponent={() => <ListDivider />}
+                    style={styles.matches}
+                    showsVerticalScrollIndicator={false}
                 />
             </View>
         </View>
