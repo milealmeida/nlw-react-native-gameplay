7c6a1d97a6e1c4f99f7181bf75a252e560195a0c
Milena <milenaalmeidadev@hotmail.com>
2021-07-07 16:54:46 -0400

    Aula 03 - In Orbit Finished

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ImageBackground, Text, View } from 'react-native';
+import { ImageBackground, Text, View, FlatList } from 'react-native';
 import { Fontisto } from '@expo/vector-icons';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
@@ -9,8 +9,27 @@ import BannerImg from '../../assets/banner.png';
 
 import { Background } from '../../components/Background';
 import { Header } from '../../components/Header';
+import { ListHeader } from '../../components/ListHeader';
+import { Member } from '../../components/Member';
+import { ListDivider } from '../../components/ListDivider';
+import { ButtonIcon } from '../../components/ButtonIcon';
 
 export function AppointmentDetails() {
+    const members = [
+        {
+            id: '1',
+            username: 'Milena',
+            avatar_url: 'https://github.com/milealmeida.png',
+            status: 'online',
+        },
+        {
+            id: '2',
+            username: 'Milena',
+            avatar_url: 'https://github.com/milealmeida.png',
+            status: 'offline',
+        },
+    ];
+
     return(
         <Background>
             <Header 
@@ -37,6 +56,24 @@ export function AppointmentDetails() {
                     </Text>
                 </View>
             </ImageBackground>    
+
+            <ListHeader
+                title="Jogadores"
+                subtitle="Total 3"
+            />
+
+            <FlatList
+                data={members}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <Member data={item} />
+                )}
+                ItemSeparatorComponent={() => <ListDivider />}
+                style={styles.members}
+            />
+            <View style={styles.footer}>
+                <ButtonIcon title="Entrar na partida" />
+            </View>
         </Background>
     );
 }
